dda92ad5babfd8119c71e948ebddbb1528faf7d9
lykmapipo <lallyelias87@gmail.com>
2019-09-23 16:56:27 +0300

    chore(seed): prepare initial event functions

---
+++ seeds/predefines/eventfunctions.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const functions = [
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Direction and Control' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Communication and Warning' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Evacuation' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Firefighting' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Law Enforcement' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Health and Medical Services' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Search and Rescue' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Shelter and Mass Care' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Emergency Public Information' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Damage Assessment' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Public Works and Engineering' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Utilities' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Resources Management and Supply' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Transportation' },
+    description: { en: '' },
+  },
+  {
+    namespace: 'EventFunction',
+    name: { en: 'Dead-Bodies Management' },
+    description: { en: '' },
+  },
+];
+
+module.exports = functions;
---
+++ seeds/predefines/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const functions = require('./eventfunctions');
+
+const predefines = [...functions];
+
+module.exports = predefines;
