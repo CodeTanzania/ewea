8999ac3367118e0bc0c9fd8cb8e457f137a31283
lykmapipo <lallyelias87@gmail.com>
2019-09-23 20:29:11 +0300

    chore(scripts): add data seed

---
+++ scripts/seedData.js
@@ -0,0 +1,28 @@
+const { waterfall } = require('async');
+const { warn, debug } = require('@lykmapipo/logger');
+const { connect, syncIndexes, Predefine } = require('../src/database');
+
+const ensureConnection = next => {
+  debug('Seeding start');
+  return connect(error => next(error));
+};
+
+const ensureIndexes = next => {
+  debug('Seeding');
+  return syncIndexes(error => next(error));
+};
+
+const seed = next => {
+  return Predefine.seed(next);
+};
+
+const tasks = [ensureConnection, ensureIndexes, seed];
+
+waterfall(tasks, (error, results) => {
+  if (error) {
+    warn('Seeding failed', error);
+  } else {
+    debug('Seeding finished', results);
+  }
+  process.exit(0);
+});
