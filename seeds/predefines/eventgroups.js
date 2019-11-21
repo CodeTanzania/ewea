07838498bc16eb45830e98e65903848660b41c34
Benson Maruchu <benmaruchu@gmail.com>
2019-11-21 18:19:34 +0300

    feat(event-group): add event group seed data

---
+++ seeds/predefines/eventgroups.js
@@ -0,0 +1,64 @@
+const groups = [
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Geological' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Meteorological' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Safety' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Security' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Rescue' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Health' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Enviromental' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Transport' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Infrastructure' },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'CBRNE' },
+    },
+  },
+];
+
+module.exports = groups;
--- seeds/predefines/index.js
+++ seeds/predefines/index.js
@@ -4,6 +4,7 @@ const severities = require('./eventseverities');
 const urgencies = require('./eventurgencies');
 const certainties = require('./eventcertainties');
 const types = require('./eventtypes');
+const groups = require('./eventgroups');
 
 const predefines = [
   ...functions,
@@ -12,6 +13,7 @@ const predefines = [
   ...urgencies,
   ...certainties,
   ...types,
+  ...groups,
 ];
 
 module.exports = predefines;
