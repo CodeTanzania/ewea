bb8ab759a64ec85555fbac72f2c904919df70ad3
Benson Maruchu <benmaruchu@gmail.com>
2019-11-25 18:37:27 +0300

    feat(event-group): add descriptions for event groups from em-dat

--- seeds/predefines/eventgroups.js
+++ seeds/predefines/eventgroups.js
@@ -2,25 +2,61 @@ const groups = [
   {
     namespace: 'EventGroup',
     strings: {
-      name: { en: 'Geological' },
+      name: { en: 'Geophysical' },
+      description: {
+        en:
+          'A hazard originating from solid earth. This term is used interchangeably with the term geological hazard',
+      },
     },
   },
   {
     namespace: 'EventGroup',
     strings: {
       name: { en: 'Meteorological' },
+      description: {
+        en:
+          'A hazard caused by short-lived, micro- to meso-scale extreme weather and atmospheric conditions that last from minutes to days',
+      },
     },
   },
   {
     namespace: 'EventGroup',
     strings: {
       name: { en: 'Hydrological' },
+      description: {
+        en:
+          'A hazard caused by the occurrence, movement, and distribution of surface and subsurface freshwater and saltwater',
+      },
     },
   },
   {
     namespace: 'EventGroup',
     strings: {
       name: { en: 'Biological' },
+      description: {
+        en:
+          'A hazard caused by the exposure to living organisms and their toxic substances (e.g. venom, mold) or vector-borne diseases that they may carry. Examples are venomous wildlife and insects, poisonous plants, and mosquitoes carrying disease-causing agents such as parasites, bacteria, or viruses (e.g. malaria)',
+      },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Extraterrestrial' },
+      description: {
+        en:
+          'A hazard caused by asteroids, meteoroids, and comets as they pass near-earth, enter the Earth’s atmosphere, and/or strike the Earth, and by changes in interplanetary conditions that effect the Earth’s magnetosphere, ionosphere, and thermosphere',
+      },
+    },
+  },
+  {
+    namespace: 'EventGroup',
+    strings: {
+      name: { en: 'Climatological' },
+      description: {
+        en:
+          'A hazard caused by long-lived, meso- to macro-scale atmospheric processes ranging from intra-seasonal to multi-decadal climate variability',
+      },
     },
   },
   {
@@ -56,19 +92,23 @@ const groups = [
   {
     namespace: 'EventGroup',
     strings: {
-      name: { en: 'Transport' },
+      name: { en: 'Transport Accident' },
     },
   },
   {
     namespace: 'EventGroup',
     strings: {
-      name: { en: 'Infrastructure' },
+      name: { en: 'Industrial Accident' },
     },
   },
   {
     namespace: 'EventGroup',
     strings: {
       name: { en: 'CBRNE' },
+      description: {
+        en:
+          'A hazard caused by Chemical, Biological, Radiological, Nuclear, and Explosive materials',
+      },
     },
   },
 ];
