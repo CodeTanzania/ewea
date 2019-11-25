a7fc72b983bff0cdd45e417e199bfa7cf8438b24
Navish <navish45@gmail.com>
2019-11-25 14:47:09 +0300

    feat(event-funtion): add more event function seed data

--- seeds/predefines/eventfunctions.js
+++ seeds/predefines/eventfunctions.js
@@ -3,105 +3,150 @@ const functions = [
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Direction and Control' },
-      description: { en: '' },
+      description: {
+        en:
+          'Directing and controlling of all emergency response activities in Dar es Salaam Region, including making sure that the response is governed by the applicable legal framework, and efforts and resources are well coordinated and implemented.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Communication and Warning' },
-      description: { en: '' },
+      description: {
+        en:
+          'Ensuring a rapid and reliable communication of official information for effective disaster response operations. Official information to be issued relates to the causes, effects, potential hazards and action to be taken to prevent death, injury or damage to property.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Evacuation' },
-      description: { en: '' },
+      description: {
+        en:
+          'Releasing of order for evacuation, controlling evacuation operations from the EOC, and moving people from the emergency area to a safer place through identified routes, and provision of return instructions.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Firefighting' },
-      description: { en: '' },
+      description: {
+        en:
+          'Managing of firefighting operations, including preventing, detecting and controlling/extinguishing fire at the disaster scene.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Law Enforcement' },
-      description: { en: '' },
+      description: {
+        en:
+          'Securing the incident area, maintaining public safety and order crime prevention, ensuring access to justice; and traffic and crowd control.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Health and Medical Services' },
-      description: { en: '' },
+      description: {
+        en:
+          'Providing treatment, stabilization and caring for those injured at the scene and transferring them to health facilities, as well as appropriate transport of medical staff, resources and equipment.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Search and Rescue' },
-      description: { en: '' },
+      description: {
+        en:
+          'Searching and rescuing of people trapped (by fire, wreckage or debris), lost or at risk.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Shelter and Mass Care' },
-      description: { en: '' },
+      description: {
+        en:
+          'Constructing temporary shelter and facilities in case of an evacuation, including their use and management. Likewise, it also entails family reunification and provision of health and medical services, emergency relief materials, food and psychosocial support.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Emergency Public Information' },
-      description: { en: '' },
+      description: {
+        en:
+          'Timely releasing and disseminating official information to the public with regards to the disaster incident, including handling of its information enquiries.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Damage Assessment' },
-      description: { en: '' },
+      description: {
+        en:
+          'Determining the magnitude of the disaster, potential associated risks and preparation of situation reports.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Public Works and Engineering' },
-      description: { en: '' },
+      description: {
+        en:
+          'Organization of resources to repair and restore essential public facilities and services (roads/streets, solid waste management, sanitary sewer and waste water treatment), removal of debris, inspection of damaged structures, and provision of engineering solutions to manage the disaster situation.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Utilities' },
-      description: { en: '' },
+      description: {
+        en:
+          'Restoring gas, clean water and wastewater services and their infrastructure in the affected areas. It also involves employing alternative measures to provide these services in times of an emergency.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Resources Management and Supply' },
-      description: { en: '' },
+      description: {
+        en:
+          'Managing of resources (financial, material and human), inventory and prompt allocation of them as needed. It implies facilitation of logistics for relief supplies, including their procurement, transport, storage, distribution and inventory.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Transportation' },
-      description: { en: '' },
+      description: {
+        en:
+          'Transporting people and resources to, from and within the affected area, as well as the internal mobility of the city. It also entails enabling the ground, marine and aerial transport that is required.',
+      },
     },
   },
   {
     namespace: 'EventFunction',
     strings: {
       name: { en: 'Dead-Bodies Management' },
-      description: { en: '' },
+      description: {
+        en:
+          'Managing of dead, including their identification, investigation on the reasons for the death and burialof bodies that could not be identified or pose a health risk. This also includes installation and management of mortuaries.',
+      },
     },
   },
 ];
