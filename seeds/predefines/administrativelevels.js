677de1e4c07ea061ec715e30ba4be54d1c7fee89
mkumbobeaty <charsbeaty@gmail.com>
2019-11-28 12:54:51 +0300

    feat(seed): add administrative levels data

---
+++ seeds/predefines/administrativelevels.js
@@ -0,0 +1,45 @@
+const levels = [
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'City' },
+    },
+    numbers: { level: 1 },
+  },
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'Municipality' },
+    },
+    numbers: { level: 2 },
+  },
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'Ward' },
+    },
+    numbers: { level: 3 },
+  },
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'Sub-ward' },
+    },
+    numbers: { level: 4 },
+  },
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'Shina' },
+    },
+    numbers: { level: 5 },
+  },
+  {
+    namespace: 'AdministrativeLevel',
+    strings: {
+      name: { en: 'Street' },
+    },
+    numbers: { level: 6 },
+  },
+];
+module.exports = levels;
--- seeds/predefines/index.js
+++ seeds/predefines/index.js
@@ -6,6 +6,7 @@ const certainties = require('./eventcertainties');
 const types = require('./eventtypes');
 const groups = require('./eventgroups');
 const featureTypes = require('./featuretypes');
+const administrativeLevels = require('./administrativelevels');
 
 const predefines = [
   ...functions,
@@ -16,6 +17,7 @@ const predefines = [
   ...types,
   ...groups,
   ...featureTypes,
+  ...administrativeLevels,
 ];
 
 module.exports = predefines;
