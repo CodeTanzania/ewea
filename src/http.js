337d3e5067a2f67be261388d8d95ef8b4e9e6ad0
lykmapipo <lallyelias87@gmail.com>
2019-11-28 17:41:45 +0300

    feat(api): expose data schemas

--- src/http.js
+++ src/http.js
@@ -1,5 +1,7 @@
 const { debug } = require('@lykmapipo/logger');
-const { app, mount, start } = require('@lykmapipo/express-common');
+const { app, get, mount, start } = require('@lykmapipo/express-common');
+const { apiVersion } = require('@lykmapipo/env');
+const { jsonSchema } = require('@lykmapipo/mongoose-common');
 const { predefineRouter } = require('@lykmapipo/predefine');
 const { permissionRouter } = require('@lykmapipo/permission');
 const {
@@ -10,6 +12,11 @@ const {
 const { connect } = require('./database');
 
 debug('Start Mounting Http Routers');
+get(`/${apiVersion()}/schemas`, (request, response) => {
+  const schema = jsonSchema();
+  response.status(200);
+  response.json(schema);
+});
 mount(authenticationRouter, predefineRouter, permissionRouter, partyRouter);
 debug('Finish Mounting Http Routers');
 
