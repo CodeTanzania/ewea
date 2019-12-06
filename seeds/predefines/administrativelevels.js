1743af64e5a913bba4fa19289b71d24bb8dc5c92
mkumbobeaty <charsbeaty@gmail.com>
2019-12-06 16:44:12 +0300

    refactor: improve administrative level

--- seeds/predefines/administrativelevels.js
+++ seeds/predefines/administrativelevels.js
@@ -23,23 +23,9 @@ const levels = [
   {
     namespace: 'AdministrativeLevel',
     strings: {
-      name: { en: 'Sub-ward' },
+      name: { en: 'Village' },
     },
     numbers: { level: 4 },
-  },
-  {
-    namespace: 'AdministrativeLevel',
-    strings: {
-      name: { en: 'Shina' },
-    },
-    numbers: { level: 5 },
-  },
-  {
-    namespace: 'AdministrativeLevel',
-    strings: {
-      name: { en: 'Street' },
-    },
-    numbers: { level: 6 },
   },
 ];
 module.exports = levels;
