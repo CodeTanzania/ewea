21c025e52165e22b9df7c8c4f990cf0687d6f858
lykmapipo <lallyelias87@gmail.com>
2019-12-18 16:44:52 +0300

    feat(seed): add event indicators

---
+++ seeds/predefines/eventindicators.js
@@ -0,0 +1,94 @@
+const indicators = [
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Damages and Losses' },
+      description: { en: 'Damages and Losses' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Affected Sectors' },
+      description: { en: 'Affected Sectors' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Population' },
+      description: { en: 'Population' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Geography' },
+      description: { en: 'Geography' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Water' },
+      description: { en: 'Water' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Sanitation' },
+      description: { en: 'Sanitation' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Food & Nutrition' },
+      description: { en: 'Food & Nutrition' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Shelter' },
+      description: { en: 'Shelter' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Livelihood' },
+      description: { en: 'Livelihood' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Protection' },
+      description: { en: 'Protection' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Education' },
+      description: { en: 'Education' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Need' },
+      description: { en: 'Need' },
+    },
+  },
+  {
+    namespace: 'EventIndicator',
+    strings: {
+      name: { en: 'Situation' },
+      description: { en: 'Situation' },
+    },
+  },
+];
+module.exports = indicators;
--- seeds/predefines/index.js
+++ seeds/predefines/index.js
@@ -1,6 +1,7 @@
 const administrativeLevels = require('./administrativelevels');
 const eventActions = require('./eventactions');
 const eventCertainties = require('./eventcertainties');
+const eventIndicators = require('./eventindicators');
 const eventFunctions = require('./eventfunctions');
 const eventGroups = require('./eventgroups');
 const eventSeverities = require('./eventseverities');
@@ -13,6 +14,7 @@ const partyRoles = require('./partyroles');
 const notificationTemplates = require('./notificationtemplates');
 
 const predefines = [
+  ...eventIndicators,
   ...eventFunctions,
   ...eventStatuses,
   ...eventSeverities,
